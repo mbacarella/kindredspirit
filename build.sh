7042f88be8e5a61b45e5486a7d1bf5bdc9a5c750
Michael Bacarella <mbac@beaver.iritu.com>
2016-12-06 10:06:56 -0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SRCS=ppdriver.ml
+
+ocamlbuild \
+    -r \
+    -use-ocamlfind \
+    -pkg core \
+    -pkg core_extended  \
+    -pkg async \
+    -pkg async_extra \
+    -pkg sexplib \
+    -pkg sexplib.syntax \
+    -pkg bitstring \
+    -pkg bitstring.syntax \
+    -syntax bitstring.syntax \
+    -syntax sexplib.syntax \
+    -tag thread \
+    -tag debug \
+    -tag bin_annot \
+    -tag short_paths \
+    -cflags "-w A-4-33-40-41-42-43-34-44" \
+    -cflags -strict-sequence \
+    $SRCS ppdriver.native
