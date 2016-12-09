8297e1848ef627ae3588c1aac6048a35591b5b6a
Michael Bacarella <michael.bacarella@gmail.com>
2016-12-08 17:38:25 -0800

    set up 2d

--- kindredspirit.ml
+++ kindredspirit.ml
@@ -7,6 +7,8 @@ let reshape ~w ~h =
 
 let display () =
   GlClear.clear [`color];
+  GlDraw.color (1.0, 1.0, 0.0);
+  GlDraw.rect (100.0, 100.0) (300.0, 200.0);
   Gl.flush ();
   Glut.swapBuffers ()
 
@@ -20,6 +22,14 @@ let gl_main () =
   let _ = Glut.init ~argv:Sys.argv in
   Glut.initDisplayMode ~depth:true ~double_buffer:true ();
   let _ = Glut.createWindow ~title:"Kindred Spirit Lighting Console" in
+
+  Glut.positionWindow ~x:200 ~y:100;
+  GlMat.mode `projection;
+  GlMat.load_identity ();
+  GlMat.ortho ~x:(0.0, 500.0) ~y:(0.0, 600.0) ~z:(-100.0, 100.0);
+  GlMat.mode `modelview;
+  GlMat.load_identity ();
+
   Glut.reshapeFunc ~cb:reshape;
   Glut.displayFunc ~cb:display;
   Glut.idleFunc ~cb:(Some Glut.postRedisplay);
