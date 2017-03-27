ea4715f60b28a0a5fe6fc71a9d45f43be170cb04
Michael Bacarella <michael.bacarella@gmail.com>
2017-03-27 09:56:20 -0700

    back to ocamlbuild

--- build.sh
+++ build.sh
@@ -1,29 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-WARN_ARGS="-w A-4-32-33-34-41-42-43"
-SRCS="color.mli color.ml \
-          pixel_pusher.mli pixel_pusher.ml \
-          color_picker.mli color_picker.ml \
-          coordinate.mli coordinate.ml \
-          virtual_pixel.mli virtual_pixel.ml \
-          model.mli model.ml \
-          animation.mli animation.ml \
-          kindredspirit.ml"
-
-ocamlfind opt \
-          $WARN_ARGS \
-          -thread \
-          -short-paths \
-          -strict-sequence \
-          -bin-annot \
-          -package ppx_fields_conv \
-          -package ppx_sexp_conv \
-          -package core \
-          -package async \
-          -package lablgl \
-          -package lablgl.glut \
-          -package bitstring \
-          -package ppx_bitstring \
-          -linkpkg \
-          -o kindredspirit.native \
-          $SRCS
+ocamlbuild \
+    -use-ocamlfind \
+    -pkg core \
+    -pkg async \
+    -pkg lablgl \
+    -pkg lablgl.glut \
+    -pkg ppx_bitstring \
+    -pkg bitstring \
+    -tag "ppx(~/.opam/4.04.0/lib/ppx_bitstring/ppx)" \
+    -tag "ppx(ppx-jane -as-ppx)" \
+    -tag thread \
+    -tag debug \
+    -tag bin_annot \
+    -tag short_paths \
+    -cflags "-w A-4-33-40-41-42-43-34-44" \
+    -cflags -strict-sequence \
+    kindredspirit.native
