18956c444e4d680f0d2b7147344b0b559dd6bc2c
Michael Bacarella <michael.bacarella@gmail.com>
2017-07-02 09:46:33 -0700

    startup script

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+EXE="kindredspirit.native"
 MY_IP=10.1.1.120/24
 DEV=enp8s0
 
-if ! (ip address show "$DEV" | grep -q "$MY_IP"); then
+if ! (/bin/ip address show "$DEV" | grep -q "$MY_IP"); then
   echo "*** Interface $DEV not configured..."
-  sudo ip address add "$MY_IP" dev "$DEV"
+  sudo /bin/ip address add "$MY_IP" dev "$DEV"
   echo "*** Added $MY_IP to $DEV"
 else
   echo "*** Interface $DEV has good address $MY_IP."
 fi
 
+echo "*** Now starting $EXE..."
 ./build.sh && ./kindredspirit.native -sound-dev default -no-beat-detection
