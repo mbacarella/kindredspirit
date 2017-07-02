924ce99ac03c45d79cb2ed76c44f9caa0ecb3682
Michael Bacarella <michael.bacarella@gmail.com>
2017-07-02 09:44:13 -0700

    forgot to git add waveform ml and mlis

---
+++ waveform.ml
@@ -0,0 +1,22 @@
+open Core
+  
+let hz = 44100
+let num_samples_per_read = hz / 100
+
+let pcm_data = Array.create ~len:100 0
+let index = ref 0
+  
+let start ~sound_dev =
+  let audio = Audio.init ~hz:44100 ~sound_dev ~num_samples_per_read in
+  let bbuf = [| Array.create ~len:num_samples_per_read 0 |] in
+  let rec loop () =
+    Audio.read audio bbuf;
+    let mean =
+      (Array.fold_right bbuf.(0) ~init:0 ~f:(fun a b ->
+        (abs a) + b))  / num_samples_per_read
+    in
+    pcm_data.(!index) <- mean;
+    index := (succ !index) mod Array.length pcm_data;
+    loop ()
+  in
+  loop ()
---
+++ waveform.mli
@@ -0,0 +1,7 @@
+open Core
+open Async
+
+val pcm_data : int Array.t
+val index : int ref
+
+val start : sound_dev:string -> unit
